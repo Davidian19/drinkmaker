e7b20e59bb13d55ade8d84624ccd851b21e9d1c6
David Ian <78829640+Davidian19@users.noreply.github.com>
2024-06-08 12:20:06 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[hash].[ext]',
+          outputPath: 'static/media/',
+          publicPath: '/_next/static/media/',
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
